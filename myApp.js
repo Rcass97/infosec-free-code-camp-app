fc755f912344136c57199856b0f729611c9fec7a
Reed Cass <98681839+Rcass97@users.noreply.github.com>
2024-01-22 17:47:11 -0700

    Fixed double listening error

--- myApp.js
+++ myApp.js
@@ -2,13 +2,6 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
- const PORT = process.env.PORT || 3030;
-
- app.listen(PORT, () => {
-  console.log(`Reed Cass Info Security Application Server started on port ${PORT}`);
- });
-
-
 
 
 
@@ -62,5 +55,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`Reed Cass Info Security Application Server started on port ${port}`);
 });
