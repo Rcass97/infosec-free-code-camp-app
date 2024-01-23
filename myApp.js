c7449945a12bc64c864ebf85496f36fa489d8b17
Reed Cass <98681839+Rcass97@users.noreply.github.com>
2024-01-22 21:16:33 -0700

    Lesson 12, BCrypt Hases

--- myApp.js
+++ myApp.js
@@ -2,15 +2,19 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-
+// Lesson 1:
 // Hackers can exploit known vulnerabilities in Express/Node if they see that your site is powered by Express. X-Powered-By: Express is sent in every request coming from Express by default. Use the helmet.hidePoweredBy() middleware to remove the X-Powered-By header.
 app.use(helmet.hidePoweredBy());
 
+
+// Lesson 2:
 // Your page could be put in a <frame> or <iframe> without your consent. This can result in clickjacking attacks, among other things. Clickjacking is a technique of tricking a user into interacting with a page different from what the user thinks it is. This can be obtained by executing your page in a malicious context, by means of iframing. In that context, a hacker can put a hidden layer over your page. Hidden buttons can be used to run bad scripts. This middleware sets the X-Frame-Options header. It restricts who can put your site in a frame. It has three modes: DENY, SAMEORIGIN, and ALLOW-FROM.
 app.use(helmet.frameguard(
   {action: 'deny'}
 ));
 
+
+// Lesson 3:
 // Cross-site scripting (XSS) is a frequent type of attack where malicious scripts are injected into vulnerable pages, with the purpose of stealing sensitive data like session cookies, or passwords.
 
 // The basic rule to lower the risk of an XSS attack is simple: “Never trust user’s input”. As a developer you should always sanitize all the input coming from the outside. This includes data coming from forms, GET query urls, and even from POST bodies. Sanitizing means that you should find and encode the characters that may be dangerous e.g. <, >.
@@ -20,12 +24,15 @@ app.use(helmet.frameguard(
 // The X-XSS-Protection HTTP header is a basic protection. The browser detects a potential injected script using a heuristic filter. If the header is enabled, the browser changes the script code, neutralizing it. It still has limited support.
 app.use(helmet.xssFilter());
 
+// Lesson 4:
 // Browsers can use content or MIME sniffing to override the Content-Type header of a response to guess and process the data using an implicit content type. While this can be convenient in some scenarios, it can also lead to some dangerous attacks. This middleware sets the X-Content-Type-Options header to nosniff, instructing the browser to not bypass the provided Content-Type.
 app.use(helmet.noSniff());
 
+// Lesson 5:
 // Some web applications will serve untrusted HTML for download. Some versions of Internet Explorer by default open those HTML files in the context of your site. This means that an untrusted HTML page could start doing bad things in the context of your pages. This middleware sets the X-Download-Options header to noopen. This will prevent IE users from executing downloads in the trusted site’s context.
 app.use(helmet.ieNoOpen());
 
+// Lesson 6:
 // HTTP Strict Transport Security (HSTS) is a web security policy which helps to protect websites against protocol downgrade attacks and cookie hijacking. If your website can be accessed via HTTPS you can ask user’s browsers to avoid using insecure HTTP. By setting the header Strict-Transport-Security, you tell the browsers to use HTTPS for the future requests in a specified amount of time. This will work for the requests coming after the initial request.
 
 var ninetyDaysInSeconds= 90*24*60*60; // Creates a variable of 90 days in the form of seconds
@@ -33,20 +40,39 @@ app.use(helmet.hsts(
   {maxAge: ninetyDaysInSeconds, force: true}
 ));
 
+
+// Lesson 7:
 // To improve performance, most browsers prefetch DNS records for the links in a page. In that way the destination ip is already known when the user clicks on a link. This may lead to over-use of the DNS service (if you own a big website, visited by millions people…), privacy issues (one eavesdropper could infer that you are on a certain page), or page statistics alteration (some links may appear visited even if they are not). If you have high security needs you can disable DNS prefetching, at the cost of a performance penalty.
 app.use(helmet.dnsPrefetchControl());
 
-
+// Lesson 8:
 // If you are releasing an update for your website, and you want the users to always download the newer version, you can (try to) disable caching on client’s browser. It can be useful in development too. Caching has performance benefits, which you will lose, so only use this option when there is a real need.
 app.use(helmet.noCache());
 
 
+// Lesson 9:
 // This challenge highlights one promising new defense that can significantly reduce the risk and impact of many type of attacks in modern browsers. By setting and configuring a Content Security Policy you can prevent the injection of anything unintended into your page. This will protect your app from XSS vulnerabilities, undesired tracking, malicious frames, and much more. CSP works by defining an allowed list of content sources which are trusted. You can configure them for each kind of resource a web page may need (scripts, stylesheets, fonts, frames, media, and so on…). There are multiple directives available, so a website owner can have a granular control. See HTML 5 Rocks, KeyCDN for more details. Unfortunately CSP is unsupported by older browsers.
 
 // By default, directives are wide open, so it’s important to set the defaultSrc directive as a fallback. Helmet supports both defaultSrc and default-src naming styles. The fallback applies for most of the unspecified directives.
 app.use(helmet.contentSecurityPolicy({
   directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'",'trusted-cdn.com']}} //Directives object configures page with default and script sources
 ))
+
+
+// Lesson 10:
+// app.use(helmet()) will automatically include all the middleware introduced above, except noCache(), and contentSecurityPolicy(), but these can be enabled if necessary. You can also disable or configure any other middleware individually, using a configuration object.
+// app.use(helmet({
+//   frameguard: { //configure
+//     action: 'deny'
+//   },
+//   contentSecurityPolicy: { //enable and configure
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       styleSrc: ['style.com'],
+//     }
+//   },
+//   dnsPrefetchControl: false //disable
+// }))
 
 
 
